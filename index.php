5e9e12832372d20af41c0098361b25fb94470e61
MZ <zukasmichael@gmail.com>
2015-02-11 12:41:01 -0500

    bootstrap envelope

--- index.php
+++ index.php
@@ -1,18 +1,68 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>youtube downloader</title>
+	<meta name="Author" content="Michael Zukas (aka Mindaugas Zukauskas)"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
 
-parse_str( file_get_contents( 'http://www.youtube.com/get_video_info?video_id=' . $_GET['id'] .'&el=detailpage' ), $info );
+<br/>
+<br/>
+<br/>
+<br/>
 
-if ( isset( $info['url_encoded_fmt_stream_map'] ) ) {
+<div class="container">
 
-	$streams = explode( ',', $info['url_encoded_fmt_stream_map'] );
+	<div class="row">
 
 
-	foreach ( $streams as $stream ) {
+		<div class="jumbotron">
+			<h1>youtube downloader (php)</h1>
+		</div>
+		<table class="table table-bordered table-striped">
+			<thead>
+			<tr>
+				<th>info</th>
+				<th>url</th>
+			</tr>
+			</thead>
+			<tbody>
 
-		parse_str( $stream, $real_stream );
-		echo "{$real_stream['quality']} {$real_stream['type']} <a href='{$real_stream['url']}' download=''>url</a><br>";
-	}
-} else {
-	echo "<pre>" . print_r( $info, true ) . "</pre>";
 
-}
+			<?php
+
+			parse_str( file_get_contents( 'http://www.youtube.com/get_video_info?video_id=' . $_GET['id'] . '&el=detailpage' ), $info );
+
+			if ( isset( $info['url_encoded_fmt_stream_map'] ) ) {
+
+				$streams = explode( ',', $info['url_encoded_fmt_stream_map'] );
+
+
+				foreach ( $streams as $stream ) {
+
+					parse_str( $stream, $real_stream );
+
+					echo "				<tr><td>{$real_stream['quality']} {$real_stream['type']}</td> <td><a href='{$real_stream['url']}' download=''>download</a></td></tr>";
+				}
+			} else {
+				echo "<pre>" . print_r( $info, true ) . "</pre>";
+
+			}
+			?>
+
+
+			</tbody>
+		</table>
+
+
+	</div>
+
+</div>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
